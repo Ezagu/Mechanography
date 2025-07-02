4b3c93cb3805dfdd70a7e91f26bd65bcd23a94be
Ezagu <agustincalpedm@gmail.com>
2025-07-02 01:07:07 -0300

    refactor

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-const wordsList = ['hello', 'back', 'cat', 'dog', 'ruin', 'socks', 'read', 'piano', 'pencil', 'red', 'white', 'clock'];
+const wordsList = ['hello', 'back', 'cat', 'dog', 'ruin', 'socks', 'read', 'piano', 'pencil', 'red', 'white', 'clock', 'perfect', 'rain', 'modify', 'single', 'incorrect', 'dinosaur', 'elephant', 'nowhere', 'who', 'by', 'it', 'route', 'mass', 'effect', 'insert', 'alone', 'crazy', 'holiday', 'future', 'annoying', 'easy', 'hard', 'move', 'flower', 'paper'];
 
 const inputElement = document.querySelector('.js-input');
 const wordsContainerElement = document.querySelector('.js-words-container');
 const timerElement = document.querySelector('.js-timer');
 const quantityInputElement = document.querySelector('.js-quantity-input');
-let actualWordElem;
-
-let intervalTimerId;
-let quantityTimer = 10;
-let wordIndex = 0;
-let time;
-let isPlaying = false;
-let isShowingResult = false;
-const quantityWords= 200;
+const resultBgElement = document.querySelector('.js-result-bg');
+const quantityWords= 500;
 const score = {
   wrongChar: 0,
   correctChar: 0,
@@ -20,41 +13,180 @@ const score = {
   correctWord: 0
 }
 
-updateQuantityTimer();
+let currentWordElem;
+let intervalTimerId;
+let quantityTimer = 60;
+let wordIndex = 0;
+let time;
+let isPlaying = false;
+
 showWordsInDisplay(quantityWords);
+
+function onKeyUpInput(event) {
+  // Put the background red on the word if the input is wrong
+  if(!compareWord()){
+    // Wrong word
+    currentWordElem.classList.add('bgc-red');
+  }
+  else {
+    // Correct word
+    currentWordElem.classList.remove('bgc-red');
+  }
+  // If one key was pressed in the input
+  if (event.key != ' ' && event.key != 'Backspace' && event.key != 'Enter') {
+    // If not playing then start the timer
+    if(!isPlaying) {
+      startTimer();
+      isPlaying = true;
+    }
+    // Compare the chars and update score
+    if(compareChar()) {
+      // Correct char
+      score.correctChar++;
+    }
+    else {
+      // Wrong char
+      score.wrongChar++;
+    }
+  }
+  else if(event.key === ' ') {
+    // If the input was a space then submit the word
+    if(getInputValue().replaceAll(' ', '') === '') {
+      // If nothing is write then dont submit
+      inputElement.value = '';
+      return;
+    }
+    submitWord();
+  }
+}
+
+function reset(){
+  isPlaying = false;
+  // Reset score
+  score.wrongChar = 0;
+  score.correctWord = 0;
+  score.correctChar = 0;
+  score.wrongWord = 0;
+
+  inputElement.value = '';
+  wordIndex = 0;
+  // Reset the words in display
+  showWordsInDisplay(quantityWords);
+  // Stop timer
+  clearInterval(intervalTimerId);
+  // Reset timer
+  updateQuantityTimer();
+  timerElement.innerHTML = quantityTimer;
+  // Scroll the display to top
+  document.querySelector('.js-display').scrollTop = 0;
+  // Set focus on input
+  inputElement.focus();
+}
+
+function startTimer() {
+  // Start the timer
+  // Get the time and put it
+  updateQuantityTimer()
+  time = quantityTimer;
+  //Start the interval
+  intervalTimerId = setInterval(function() {
+    if(time > 0) {
+      time--;
+      timerElement.innerHTML = time;
+    }
+    if(time === 0) {
+      // Time up
+      isPlaying = false;
+      calculateUpdateResults();
+      // Stop the interval
+      clearTimeout(intervalTimerId);
+    }
+  }, 1000)
+}
+
+function updateQuantityTimer() {
+  // Get the value in the input quantity and put it in the variable
+  quantityTimer = Number(quantityInputElement.value) || 60;
+
+  if(quantityTimer < 30) {
+    // If was an incorrect value, put it 30
+    quantityInputElement.value = 30;
+    quantityTimer = 30;
+  }
+}
+
+function nextWord() {
+  // Change to the next word in the list
+  currentWordElem.classList.remove('js-actual-word');
+  wordIndex++;
+  currentWordElem = document.querySelector(`#word${wordIndex}`);
+  currentWordElem.classList.add('js-actual-word');
+  // Move the display scroll to center the actual word
+  currentWordElem.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });
+}
+
+function submitWord(){
+  // Update the score and paint the word 
+  if(compareWord() && getCurrentWord().length === getInputValue().length - 1){
+    // correct word
+    score.correctWord++;
+    currentWordElem.classList.add('green-color');
+  } else {
+    // wrong word
+    score.wrongWord++;
+    currentWordElem.classList.add('red-color');
+    currentWordElem.classList.remove('bgc-red');
+  }
+
+  // Reset the input and next word
+  inputElement.value = '';
+  nextWord();
+}
+
+function compareWord() {
+  // Compare the actual word and the input word
+  const inputWord = getInputValue().replaceAll(' ', '');
+  const actualWord = getCurrentWord().slice(0, inputWord.length);
+  return inputWord === actualWord ? true : false;
+}
+
+function compareChar() {
+  const indexLastChar = getInputValue().length - 1;
+  const inputChar = getInputValue()[indexLastChar];
+  const actualWordChar = getCurrentWord()[indexLastChar];
+  return inputChar === actualWordChar ? true : false;
+}
+
+function getCurrentWord() {
+  return document.querySelector('.js-actual-word').innerHTML;
+}
+
+function getInputValue() {
+  return inputElement.value;
+}
+
+//-------------DOM--------------
 
 function showWordsInDisplay(quantity){
   // Create al the words and show it on the display
   let displayHtml = '';
   for(let i = 0; i < quantity; i++) {
+    // Get a random word
     const word = wordsList[Math.floor(Math.random() * wordsList.length)];
     if(i === 0) {
+      // If it is the first word then put it as actual word
       displayHtml += `<div id="word0" class="word js-actual-word">${word}</div>`;
       continue;
     }
+    // Generate html
     displayHtml += `<div id="word${i}" class="word">${word}</div>`;
   }
   wordsContainerElement.innerHTML = displayHtml;
-  actualWordElem = document.querySelector('#word0');
+  currentWordElem = document.querySelector('#word0');
 }
 
-function nextWord() {
-  // Change to the next word in the list
-  actualWordElem.classList.remove('js-actual-word');
-
-  wordIndex++;
-  if(wordIndex === quantityWords) {
-    // If it was the last word, finish
-    displayResults();
-    return;
-  }
-  actualWordElem = document.querySelector(`#word${wordIndex}`);
-  actualWordElem.classList.add('js-actual-word');
-
-  actualWordElem.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });
-}
-
-function displayResults() {
+function calculateUpdateResults() {
+  // Calculate and update the results
   const keystrokesElem = document.querySelector('.js-keystrokes');
   const accuracyElem = document.querySelector('.js-accuracy');
   const correctWordsElem = document.querySelector('.js-correct-words');
@@ -70,128 +202,21 @@ function displayResults() {
   accuracyElem.innerHTML = `${accuracy}%`
   correctWordsElem.innerHTML = `<span class="green-color">${score.correctWord}</span>`;
   wrongWordsElem.innerHTML = `<span class="red-color">${score.wrongWord}</span>`;
-  
+  // Unfocus the input
   inputElement.blur();
+  // Show the results
   toggleDisplayResult();
-}
-
-function onKeyUpInput(event) {
-  if(isShowingResult) return;
-
-  // Put the background red on the word if the input is wrong
-  if(!compareWord()){
-    actualWordElem.classList.add('bgc-red');
-  }
-  else {
-    actualWordElem.classList.remove('bgc-red');
-  }
-
-  // If one key was pressed in the input
-  if (event.key != ' ' && event.key != 'Backspace' && event.key != 'Enter') {
-    // If not playing then start the timer
-    if(!isPlaying) {
-      startTimer();
-      isPlaying = true;
-    }
-
-    // Compare the chars and update score
-    if(compareChar()) {
-      score.correctChar++;
-    }
-    else {
-      score.wrongChar++;
-    }
-  }
-  else if(event.key === ' ') {
-    // If the input was a space, submit the word
-    if(getInputValue().replaceAll(' ', '') === '') {
-      // If nothing is write, not submit
-      inputElement.value = '';
-      return;
-    }
-    submitWord();
-  }
-}
-
-function submitWord(){
-  // Update the score and paint the word 
-  if(compareWord() && getActualWord().length === getInputValue().length - 1){
-    // correct word
-    score.correctWord++;
-    actualWordElem.classList.add('green-color');
-  } else {
-    // wrong word
-    score.wrongWord++;
-    actualWordElem.classList.add('red-color');
-    actualWordElem.classList.remove('bgc-red');
-  }
-
-  // Reset the input and next word
-  inputElement.value = '';
-  nextWord();
-}
-
-function compareWord() {
-  // Compare the actual word and the input word
-  const inputWord = getInputValue().replaceAll(' ', '');
-  const actualWord = getActualWord().slice(0, inputWord.length);
-  return inputWord === actualWord ? true : false;
-}
-
-function compareChar() {
-  // Compare the last char in input with the actual word char
-  const indexLastChar = getInputValue().length - 1;
-  const inputChar = getInputValue()[indexLastChar];
-  const actualWordChar = getActualWord()[indexLastChar];
-  return inputChar === actualWordChar ? true : false;
-}
-
-function reset(){
-  isPlaying = false;
-  score.wrongChar = 0;
-  score.correctWord = 0;
-  score.correctChar = 0;
-  score.wrongWord = 0;
-  inputElement.value = '';
-  wordIndex = 0;
-  showWordsInDisplay(quantityWords);
-  clearInterval(intervalTimerId);
-  updateQuantityTimer();
-  timerElement.innerHTML = quantityTimer;
-  document.querySelector('.js-display').scrollTop = 0;
-  inputElement.focus();
-}
-
-function startTimer() {
-  // Start the timer
-  updateQuantityTimer()
-  time = quantityTimer;
-  intervalTimerId = setInterval(function() {
-    if(time > 0) {
-      time--;
-      timerElement.innerHTML = time;
-    }
-    if(time === 0) {
-      isPlaying = false;
-      displayResults();
-      clearTimeout(intervalTimerId);
-    }
-  }, 1000)
 }
 
 function toggleDisplayResult() {
   // Show or not the result modal window
-  if(document.querySelector('.js-result-bg').style.display === 'block') {
+  if(resultBgElement.style.display === 'block') {
     //Hide
-    document.querySelector('.js-result-bg')
-    .style.display = 'none';
-    isShowingResult = false;
+    resultBgElement.style.display = 'none';
   }
   else {
     //Show
-    document.querySelector('.js-result-bg')
-    .style.display = 'block';
-    isShowingResult = true;
+    resultBgElement.style.display = 'block';
   }
 }
 
@@ -200,20 +225,4 @@ function resetKeyPressed(event) {
   if(event.key === 'Enter') {
     reset();
   }
-}
-
-function updateQuantityTimer() {
-  quantityTimer = Number(quantityInputElement.value) || 60;
-  if(quantityTimer < 30) {
-    quantityInputElement.value = 30;
-    quantityTimer = 30;
-  }
-}
-
-function getActualWord() {
-  return document.querySelector('.js-actual-word').innerHTML;
-}
-
-function getInputValue() {
-  return inputElement.value;
 }
